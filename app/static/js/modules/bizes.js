3b95f87bf7d48fee455e207a48a2eb4f4b28fcf9
Eldar Dadashov <eldar-dadashov@mail.ru>
2020-12-06 05:16:17 +0300

    Сверстал несколько страниц

--- app/static/js/modules/bizes.js
+++ app/static/js/modules/bizes.js
@@ -12,24 +12,25 @@ export function bizes() {
             direction: 'vertical',
             spaceBetween: 20,
             allowTouchMove: false,
-            loop: true,
-            arrows: false,
             navigation: {
                 prevEl: '.bizes-slider__slider-prev',
                 nextEl: '.bizes-slider__slider-next'
             },
             breakpoints: {
                 720: {
+                    loop: true,
                     slidesPerView: 2,
                     direction: 'horizontal',
                     allowTouchMove: true,
                 },
                 1440: {
+                    loop: true,
                     slidesPerView: 4,
                     direction: 'horizontal',
                     allowTouchMove: true,
                 },
                 1100: {
+                    loop: true,
                     slidesPerView: 3,
                     direction: 'horizontal',
                     allowTouchMove: true,
