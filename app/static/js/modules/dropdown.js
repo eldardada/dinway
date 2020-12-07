2434c8a52f348e06059f6b306b9f76af26bf7cfc
Eldar Dadashov <eldar-dadashov@mail.ru>
2020-12-07 14:18:40 +0300

    Добавил сменяемость языка в списке

--- app/static/js/modules/dropdown.js
+++ app/static/js/modules/dropdown.js
@@ -1,10 +1,21 @@
 export function dropdown() {
     document.addEventListener('click', e => {
         const item = e.target.closest('[data-list-item]');
-        const nested = item ? item.querySelector('[data-list]') : false;
-    
+        let nested = null;
+        
+        if(item) {
+            nested = item ? item.querySelector('[data-list]') : false;
+        }
+        
         if(nested) {
             nested.classList.toggle('active');
+            const title = item.querySelector('[data-title]');
+            const select = nested.querySelector('[data-select]');
+            if(title && select && e.target === select) {
+                let tmp = title.textContent;
+                title.textContent = select.textContent;
+                select.textContent = tmp;
+            }
         }
         else {
             document.querySelectorAll('[data-list]').forEach(el => {
